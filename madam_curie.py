e9492a9ba9a245695898a93df5b1e86bcfad454d
Justin Reese <justaddcoffee@gmail.com>
2021-07-20 17:39:59 -0400

    catch some CORD CURIES

--- madam_curie.py
+++ madam_curie.py
@@ -5,6 +5,9 @@ import yaml
 from tqdm import tqdm
 import gzip
 
+# should prefixes in CURIEs and mappings be upcased and cases therefore ignored?
+upcase = True
+
 # CURIE mappings, in descending order of priority
 sources = [
     ['dipper', 'https://raw.githubusercontent.com/monarch-initiative/dipper/master/dipper/curie_map.yaml'],
@@ -15,15 +18,19 @@ for m in tqdm(sources, "making map"):
     y = yaml.safe_load(requests.get(m[1]).text)
     if m[0] == 'biolink':
         y = y['prefixes']
-    mapping.append({k: v for k, v in y.items()})
+    mapping.append({k.upper(): v for k, v in y.items()}) if upcase else mapping.append({k: v for k, v in y.items()})
 
 
 def curie_to_url(curie) -> str:
     if re.match("^[^:]+:[^:]+$", line[0]):
         prefix, body = curie.split(":")
+        if upcase:
+            prefix = prefix.upper()
         for m in mapping:
             if prefix in m:
                 return m[prefix] + body
+            if prefix == "CORD" and bool(re.match("^PMC", body)):
+                return 'https://www.ncbi.nlm.nih.gov/pmc/articles/' + body
     return curie
 
 
